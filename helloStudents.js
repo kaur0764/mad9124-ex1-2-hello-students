c6e5b60357f135a90570bc00a359b97e1d79cfce
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-01-11 12:58:36 +0530

    prints hello with full names and number of lastnames starting with 'D'

--- helloStudents.js
+++ helloStudents.js
@@ -7,3 +7,16 @@
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+
+const students = require('./students.json')
+
+let count = 0
+
+students.forEach(({firstName, lastName}) => {
+  console.log(`Hello ${firstName} ${lastName}`)
+  if(lastName.startsWith('D')){
+    count++
+  }
+})
+
+console.log(`Count of last names starting with D is ${count}`)
